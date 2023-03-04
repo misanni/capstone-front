a1eedc4baacef12440c8e50476967ebd25df2cdf
MARY SANNI <marysanni@MARYs-MacBook-Pro.local>
2023-03-04 07:54:15 -0600

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,26 +3,9 @@ import "../styles/Header.css";
 
 function Header(props) {
   return (
-    <section>
-      <nav className="nav">
-        <div className="siteName">
-          <Link to="/" className="jobifyHeader">
-            <h1>Jobify</h1>
+    <Link to="/" >
+            <h1>Ecommerce App</h1>
           </Link>
-        </div>
-
-        <div className="links">
-          <div className="createPost">
-            <Link className="linkBtn" to="/posts/new">Create Post</Link>
-          </div>
-          <div className="allPosts">
-            <Link className="APLink" to="/posts">
-           <button className="APBtn">All Posts</button>
-            </Link>
-          </div>
-        </div>
-      </nav>
-    </section>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,16 +8,14 @@ const goals = useLoaderData()
         <section className="homePage">
             <section class="homeBody">
                 <section className="homeText">
-                    <h2>A Place For All Your <br/>Job goals</h2>
+                    <h2>Shopping app<br/>shop</h2>
                   </section>
-                <section className="homeGraphic">
-                    <img src="https://i.imgur.com/SQkvSRO.png" alt="something" />
-                </section>
+                
             </section>
             <section className="additionsSect">
                 <h3>Recent Additions</h3>
                 <section className="jobCards">
-                    {goals.slice(-2).map((goal) => (
+                    {goals.slice(-4).map((goal) => (
                          <Link to={`/goals/${goal._id}`}>
                          <div className="jobCard">
                         <h4>{goal.text}</h4>
